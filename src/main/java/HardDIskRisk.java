2c64c3a0d6a81f4d00f314595b901d3cb013dfb8
Jake <e12123437@student.tuwien.ac.at>
2022-11-19 00:03:23 +0100

    Maybe it works?

--- src/main/java/HardDIskRisk.java
+++ src/main/java/HardDIskRisk.java
@@ -1,6 +1,22 @@
-public class HardDIskRisk {
+import at.ac.tuwien.ifs.sge.agent.*;
+import at.ac.tuwien.ifs.sge.engine.Logger;
+import at.ac.tuwien.ifs.sge.game.Game;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class HardDIskRisk <G extends Game<A, ?>, A> extends AbstractGameAgent<G, A> implements GameAgent<G, A> {
 
 
+        public HardDIskRisk(Logger log){
+            super(log);
+        }
+
+    @Override
+    public A computeNextAction(G game, long computationTime, TimeUnit timeUnit) {
+        //optionally set AbstractGameAgent timers
+        super.setTimers(computationTime, timeUnit);
+    //choose the first option
+        return List.copyOf(game.getPossibleActions()).get(0);
+    }
 }
-
-
