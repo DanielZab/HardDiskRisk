71275c9f1d5e2a16835c48f5dcbdb7afef9a9289
Daniel Zabielski <e12119018@student.tuwien.ac.at>
2023-01-06 18:39:08 +0100

    bug Fixed?

--- src/main/java/HardDiskRisk.java
+++ src/main/java/HardDiskRisk.java
@@ -58,7 +58,7 @@ public class HardDiskRisk extends AbstractGameAgent<Risk, RiskAction> implements
     public static ArrayList<Set<Integer>> staticContinents = new ArrayList<>(Arrays.asList(new HashSet<Integer>(Arrays.asList(38,39,40,41)), new HashSet<Integer>(Arrays.asList(9,10,11,12)), new HashSet<Integer>(Arrays.asList(0,1,2,3,4,5,6,7,8)),
     new HashSet<Integer>(Arrays.asList(13,14,15,16,17,18,19)), new HashSet<Integer>(Arrays.asList(20,21,22,23,24,25)), new HashSet<Integer>(Arrays.asList(26,27,28,29,30,31,32,33,34,35,36,37))));
 
-    public ArrayList<Set<Integer>> continents;
+    public ArrayList<Set<Integer>> continents = new ArrayList<>();
     // Custom variables and functions
 
     private int placedTroupsCounter = 0;
